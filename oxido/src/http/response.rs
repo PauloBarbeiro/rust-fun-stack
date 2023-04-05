890310fdc3dc47b6a8efadc675a4390718f3d2e4
paulo.barbeiro <msg@paulobf.dev>
2023-04-05 23:33:29 +0200

    implement tests for response and satatus_code

--- oxido/src/http/response.rs
+++ oxido/src/http/response.rs
@@ -55,3 +55,32 @@ pub mod response {
         }
     }
 }
+
+#[cfg(test)]
+mod describe_response {
+    use super::response::Response;
+    use super::super::StatusCode;
+
+    #[test]
+    fn should_sent_response_without_body(){
+        let mut out = Vec::new();
+
+        Response::new(StatusCode::NotFound, None).send(&mut out).unwrap();
+
+        let string_output = String::from_utf8(out).unwrap();
+        assert_eq!(string_output, "HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
+    #[test]
+    fn should_sent_response_with_body(){
+        let mut out = Vec::new();
+
+        Response::new(
+            StatusCode::Ok,
+            Some("<h1>Test body</h1>".to_string())
+        ).send(&mut out).unwrap();
+
+        let string_output = String::from_utf8(out).unwrap();
+        assert_eq!(string_output, "HTTP/1.1 200 Ok\r\n\r\n<h1>Test body</h1>");
+    }
+}
--- oxido/src/http/status_code.rs
+++ oxido/src/http/status_code.rs
@@ -5,6 +5,9 @@ pub mod status_code {
         Result as FmtResult
     };
 
+    /// StatusCode:
+    /// Defines the behavior and functionality needed for the use of http status code
+    /// in the HTTP server.
     #[derive(Copy, Clone, Debug)]
     pub enum StatusCode {
         Ok = 200,
@@ -26,5 +29,48 @@ pub mod status_code {
         fn fmt(&self, f: &mut Formatter) -> FmtResult {
             write!(f, "{}", *self as u16)
         }
+    }
+}
+
+#[cfg(test)]
+mod describe_reason_phrases {
+    use super::status_code::StatusCode;
+
+    #[test]
+    fn should_return_ok() {
+        assert_eq!(StatusCode::Ok.reason_phrase(), "Ok");
+    }
+
+    #[test]
+    fn should_return_bad_request() {
+        let code = StatusCode::BadRequest;
+        assert_eq!(code.reason_phrase(), "Bad Request");
+    }
+
+    #[test]
+    fn should_return_not_found() {
+        let code = StatusCode::NotFound;
+        assert_eq!(code.reason_phrase(), "Not Found");
+    }
+}
+
+
+#[cfg(test)]
+mod describe_status_code_display {
+    use super::status_code::StatusCode;
+
+    #[test]
+    fn should_render_200() {
+        assert_eq!(StatusCode::Ok.to_string(), "200");
+    }
+
+    #[test]
+    fn should_render_400() {
+        assert_eq!(StatusCode::BadRequest.to_string(), "400");
+    }
+
+    #[test]
+    fn should_render_404() {
+        assert_eq!(StatusCode::NotFound.to_string(), "404");
     }
 }
